801feea5b1c06cb3989fcb8385e61dd5c2b48ae6
saurabhgore2007 <saurabhgore44@gmail.com>
2026-07-14 21:35:45 +0530

    Logger Example

---
+++ src/Day26/LoggingExample.java
@@ -0,0 +1,17 @@
+package Day26;
+
+import java.util.logging.Logger;
+
+public class LoggingExample {
+
+	private static final Logger logger = Logger.getLogger(LoggingExample.class.getName());
+	
+	public static void main(String[] args) {
+
+		 logger.info("Application Started");
+		 logger.warning("Low Memory");
+		 logger.severe("Database Connection Failed");
+
+	}
+
+}
